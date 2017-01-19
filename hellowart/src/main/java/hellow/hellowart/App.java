71707ff715e3d3383bbbd930c63d30ab5c772c55
Aranel Doriath <aranelofdoriath@gmail.com>
2017-01-19 13:33:40 +0200

    added maven

---
+++ hellowart/src/main/java/hellow/hellowart/App.java
@@ -0,0 +1,13 @@
+package hellow.hellowart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hll fkng wrld!" );
+    }
+}
